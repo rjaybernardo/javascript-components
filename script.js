920181f92a2ccb6be6182992bdc2b37ac4ef97ef
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-10-30 14:58:09 +0800

    tabs

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
-
-modalBtn.addEventListener("click", function () {
-  modal.classList.add("open-modal");
-});
-closeBtn.addEventListener("click", function () {
-  modal.classList.remove("open-modal");
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
 });
